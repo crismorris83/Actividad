2cc38e1f4b0c518f5ad5bccda39dfa9d20249abe
davincho <agudelodavid88@gmail.com>
2022-05-24 11:44:47 -0500

    jorge

---
+++ js/js.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById("alerta").onclick= function () {
+    alert("esta es la alerta");
+}
+document.getElementById("alerta2").onclick= function () {
+    alert("esta es la alerta");
+}
+document.getElementById("alerta3").onclick= function () {
+    alert("esta es la alerta");
+}
